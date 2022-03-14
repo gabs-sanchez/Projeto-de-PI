42f7a3a6b85faa63f9924d54ccc30007ad463f62
RafaelRpso <rafaelalvesraposo@hotmail.com>
2022-03-14 11:11:06 -0300

    comentarios 2 sniff

--- Arquitetura_Computacional/ArqComp_Sistemas/leitorUmidadeTemperatura.ino
+++ Arquitetura_Computacional/ArqComp_Sistemas/leitorUmidadeTemperatura.ino
@@ -1,4 +1,4 @@
-o in da#include "DHT.h"
+#include "DHT.h"
 #define dht_type DHT11 // define qual o tipo de sensor DHT11 que está utilizando
 int dht_pin= A2; 
 DHT dht_1 = DHT(dht_pin, dht_type); // pode configurar sensores DHTxx
@@ -7,25 +7,25 @@ void setup()
 {
 Serial.begin(9600);
 dht1.begin(); 
-  
+ 	// declaração da quantidade de dados que entra por segundo (serial begin) e do sensor (dht1)  
 }
 
 void loop()
 {
-  float umidade= dht_1.readHumidity(); 
-  float temperatura = dht_1.readTemperature(); 
-  if (isnan(temperatura) or isnan(umidade))
+  float umidade= dht_1.readHumidity();  // a variavel umidade recebe o valor do sensor de umidade
+  float temperatura = dht_1.readTemperature();  // a variavel temperatura recebe o valor de temperatura do sensor
+  if (isnan(temperatura) or isnan(umidade)) // se nao for um NUMERO, imprima ERRO
   {
     Serial.println("Erro ao ler Temperatura");
   }
-  else
+  else // se a proposicao acima for falsa, faça isso: 
   {
-  Serial.print("Umidade: ");
-  Serial.print(umidade); 
+  Serial.print("Umidade: "); 
+  Serial.print(umidade);  // imprime umidade (valor do sensor) 
   Serial.print(" Temperatura: ");
-  Serial.print(temperatura); 
+  Serial.print(temperatura);  // imprime temperatura (valor do sensor) 
   Serial.println("°C") ; 
   }
-delay(2000); 
+delay(2000);  // delay pra não fazer 4000000 impressões em 1 segundo
 
 }
