ae9094e65b3a4129797cfc9b2928da57e0eec05e
RafaelRpso <rafaelalvesraposo@hotmail.com>
2022-03-14 10:01:18 -0300

    comentarios ao codigo de arduino baseado noq entendi

--- Arquitetura_Computacional/ArqComp_Sistemas/leitorProximidade.ino
+++ Arquitetura_Computacional/ArqComp_Sistemas/leitorProximidade.ino
@@ -1,19 +1,19 @@
-int pinoSensor = 7; 
+int pinoSensor = 7; //Declara a variável.
 
 void setup(){
-  pinMode(pinoSensor, INPUT); 
-  Serial.begin(9600); 
-}
+  pinMode(pinoSensor, INPUT); //Detecta o Sensor como  INPUT na variavel pino
+  Serial.begin(9600); // detecta a quantidade de coiso por segundo 
+}   
 
-void loop(){ 
-  if(digitalRead(pinoSensor)==LOW){ 
-    Serial.println("Objeto Detectado! (1)") ; 
+void loop(){ // creio que seja loop porque fica repetindo
+  if(digitalRead(pinoSensor)==LOW){ //se ESTA ATIVADO == PESSOA DETECTADA 
+    Serial.println("Objeto Detectado! (1)") ; // imprime 'OBJETO DETECTADO' 
   }
-  else if(isnan(pinoSensor)) {
-    Serial.print("Erro!");
-    break; 
+  else if(isnan(pinoSensor)) {  //se o VALOR NAO E UM NUMERO == ERRO NO SIS.
+    Serial.print("Erro!"); // IMPRIME ERRO
+    break; // ENCERRA O LOOP
   }
-  else { 
-    Serial.println("Objeto ausente (0)");
+  else { // SE nenhum dos dois ifs forem verdadeiro, faça: 
+    Serial.println("Objeto ausente (0)");  // imprime objeto ausente
   }
 }
