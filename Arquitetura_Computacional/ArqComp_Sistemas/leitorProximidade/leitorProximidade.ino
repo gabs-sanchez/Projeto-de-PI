6b70c9c6cb25bf40c696f0138d7bcd853f03cd1d
RafaelRpso <rafaelalvesraposo@hotmail.com>
2022-03-14 10:43:02 -0300

    comentarios à respeito do nosso sensor de movimento

--- Arquitetura_Computacional/ArqComp_Sistemas/leitorProximidade/leitorProximidade.ino
+++ Arquitetura_Computacional/ArqComp_Sistemas/leitorProximidade/leitorProximidade.ino
@@ -1,20 +1,12 @@
-#include <DTH.h>
-#include <DTH_U.h>
-#include <Adafruit_Sensor.h> //Chamado das bibliotecas
-
-
 int pinoSensor = 7; //Declara a variável.
 
 void setup(){
   pinMode(pinoSensor, INPUT); //Detecta o Sensor como  INPUT na variavel pino
   Serial.begin(9600); // detecta a quantidade de coiso por segundo 
-}   // taxa de transmisssao de dados = Serial.Begin(9600)
+}   
 
 void loop(){ // creio que seja loop porque fica repetindo
-
-	// dentro do loop, para que repita diversas vezes, se coloca o código
-
-	if(digitalRead(pinoSensor)==LOW){ //se ESTA ATIVADO == PESSOA DETECTADA 
+  if(digitalRead(pinoSensor)==LOW){ //se ESTA ATIVADO == PESSOA DETECTADA 
     Serial.println("Objeto Detectado! (1)") ; // imprime 'OBJETO DETECTADO' 
   }
   else if(isnan(pinoSensor)) {  //se o VALOR NAO E UM NUMERO == ERRO NO SIS.
